ff57aa3742732fdcb8f0430f2976caaaa02ce5f9
satish-baghel <satish769843@gmail.com>
2023-09-06 10:38:21 +0530

    worked on add listing

--- src/views/Post/Components/EditableForm.js
+++ src/views/Post/Components/EditableForm.js
@@ -1,23 +1,99 @@
+/* eslint-disable no-debugger */
+import { Button, Card, Grid, TextField } from '@mui/material';
+import { IconTrash } from '@tabler/icons';
 import { useRef } from 'react';
 import ContentEditable from 'react-contenteditable';
 
 //
-const EditableForm = ({ data }) => {
+const EditableForm = ({ data, onChange, index, onChangeFile, addMoreList }) => {
     const contentEditable = useRef();
 
-    const handleChange = (e) => {
-        console.log(e.target.value);
-    };
-    return (
-        <ContentEditable
-            innerRef={contentEditable}
-            html={data.text} // innerHTML of the editable div
-            disabled={false} // use true to disable editing
-            onChange={handleChange} // handle innerHTML change
-            tagName={data.tagName} // Use a custom HTML tag (uses a div by default)
-            style={{ padding: '10px', border: 'none' }}
-        />
-    );
+    if (!['list', 'img'].includes(data.tagName)) {
+        return (
+            <>
+                <Grid item xs={11} sm={11}>
+                    <Card variant="outlined" sx={{ px: 1, mt: 1 }}>
+                        <ContentEditable
+                            innerRef={contentEditable}
+                            html={data.text} // innerHTML of the editable div
+                            disabled={false} // use true to disable editing
+                            onChange={(e) => onChange(e, index, data.tagName)} // handle innerHTML change
+                            tagName={data.tagName} // Use a custom HTML tag (uses a div by default)
+                            style={{ padding: '10px' }}
+                            placeholder="Enter here ...."
+                        />
+                    </Card>
+                </Grid>
+                <Grid item xs={1} sm={1}>
+                    <Button variant="contained" component="label" sx={{ mx: 1, my: 'auto' }} color="error">
+                        <IconTrash />
+                    </Button>
+                </Grid>
+            </>
+        );
+    }
+    if (['img'].includes(data.tagName)) {
+        return (
+            <>
+                <Grid item xs={11} sm={11}>
+                    <Card variant="outlined" sx={{ px: 1, mt: 1 }} style={{ width: '100%' }}>
+                        <Button variant="contained" component="label" sx={{ my: 1 }}>
+                            Upload File
+                            <input type="file" hidden onChange={(e) => onChangeFile(e, index)} />
+                        </Button>
+                        <TextField value={data.text} onChange={(e) => onChange(e, index, data.tagName)} sx={{ ml: 1, width: '80%' }} />
+                    </Card>
+                    {data.imgUrl ? (
+                        <Card variant="outlined" sx={{ px: 1, mt: 1 }}>
+                            <img src={data.imgUrl} alt="imag" width={100} />
+                        </Card>
+                    ) : null}
+                </Grid>
+                <Grid item xs={1} sm={1}>
+                    <Button variant="contained" component="label" sx={{ mx: 1, my: 'auto' }} color="error">
+                        <IconTrash />
+                    </Button>
+                </Grid>
+            </>
+        );
+    }
+    if (['ul'].includes(data.tagName)) {
+        return (
+            <>
+                <Grid item xs={12} sm={12} sx={{ display: 'flex', justifyContent: 'end', my: 1 }}>
+                    <Grid item xs={1} sm={1}>
+                        <Button variant="contained" component="label" sx={{ mx: 1, my: 'auto' }} onClick={() => addMoreList(index)}>
+                            Add
+                        </Button>
+                    </Grid>
+                </Grid>
+                {Array.isArray(data.text) && data.text.length
+                    ? data.text.map((list, lIndex) => (
+                          <>
+                              <Grid item xs={11} sm={11}>
+                                  <Card variant="outlined" sx={{ px: 1, mt: 1 }}>
+                                      <ContentEditable
+                                          innerRef={contentEditable}
+                                          html={list.text} // innerHTML of the editable div
+                                          disabled={false} // use true to disable editing
+                                          onChange={(e) => onChange(e, index, data.tagName, lIndex)} // handle innerHTML change
+                                          tagName={list.tagName} // Use a custom HTML tag (uses a div by default)
+                                          style={{ padding: '10px' }}
+                                      />
+                                  </Card>
+                              </Grid>
+                              <Grid item xs={1} sm={1}>
+                                  <Button variant="contained" component="label" sx={{ mx: 1, my: 'auto' }} color="error">
+                                      <IconTrash />
+                                  </Button>
+                              </Grid>
+                          </>
+                      ))
+                    : null}
+            </>
+        );
+    }
+    return null;
 };
 
 export default EditableForm;
--- src/views/Post/PostForms/DescriptionForm.js
+++ src/views/Post/PostForms/DescriptionForm.js
@@ -2,55 +2,152 @@ import { Button, Card, FormControl, Grid, InputLabel, MenuItem, Select, Stack, T
 import { useState } from 'react';
 import AnimateButton from 'ui-component/extended/AnimateButton';
 import EditableForm from '../Components/EditableForm';
+import _ from 'lodash';
 
 const tagList = [
     {
-        label: 'img',
-        value: 'Image'
+        value: 'img',
+        label: 'Image'
     },
     {
-        label: 'p',
-        value: 'Paragraph'
+        value: 'p',
+        label: 'Paragraph'
     },
     {
-        label: 'h2',
-        value: 'Heading'
+        value: 'h2',
+        label: 'Heading'
     },
     {
-        label: 'List',
-        value: 'ul'
+        value: 'ul',
+        label: 'List'
     }
 ];
-const DescriptionForm = ({ paymentData, setPaymentData, handleNext, handleBack, setErrorIndex }) => {
+const DescriptionForm = ({ handleBack, setErrorIndex }) => {
+    const [tagName, setTagName] = useState('');
+
     const [blocks, setBlocks] = useState([
         {
             text: 'This is First paragraph',
             tagName: 'p'
         }
     ]);
+
+    const onAddMore = () => {
+        let obj = { tagName, text: '' };
+        if (tagName === 'img') {
+            obj = { tagName, imgUrl: '', text: '' };
+        }
+        if (tagName === 'ul') {
+            obj = {
+                tagName,
+                text: [
+                    {
+                        tagName: 'li',
+                        text: ''
+                    }
+                ]
+            };
+        }
+        setBlocks([...blocks, obj]);
+    };
+    const addMoreList = (index) => {
+        const obj = { tagName: 'li', text: '' };
+
+        const list = blocks.map((data, i) => {
+            if (i === index) {
+                return {
+                    ...data,
+                    text: [...data.text, obj]
+                };
+            }
+
+            return data;
+        });
+        setBlocks(list);
+    };
+    const onChange = (v, index, tagName, listingIndex) => {
+        const data = blocks.map((data, i) => {
+            const obj = data;
+
+            if (index === i && data.tagName === tagName) {
+                if (Array.isArray(obj.text)) {
+                    return {
+                        ...obj,
+                        text: data.text.map((l, i) => {
+                            if (i === listingIndex) {
+                                return {
+                                    ...l,
+                                    text: v.target.value
+                                };
+                            }
+
+                            return l;
+                        })
+                    };
+                }
+                return {
+                    ...obj,
+                    text: v.target.value
+                };
+            }
+            return obj;
+        });
+        setBlocks(data);
+        // setBlocks((prev)=>{
+
+        // })
+    };
+
+    //
+    const onChangeFile = async (v, index) => {
+        const files = v.target.files;
+
+        const imagePromises = Array.from(files).map(
+            (file) =>
+                new Promise((resolve) => {
+                    const reader = new FileReader();
+                    reader.onload = (e) => resolve(e.target.result);
+                    reader.readAsDataURL(file);
+                })
+        );
+        let res = await Promise.all(imagePromises);
+        res = _.first(res);
+        const data = blocks.map((data, i) => {
+            const obj = data;
+            if (index === i) {
+                obj.imgUrl = res;
+            }
+
+            return obj;
+        });
+        setBlocks(data);
+    };
     return (
         <div>
             {' '}
-            <Grid container sx={{ my: 3, ml: 1 }}>
+            <Grid container sx={{ my: 3, ml: 1, justifyContent: 'end' }}>
                 <Grid item xs={4} sm={4}>
                     <FormControl fullWidth>
                         <InputLabel id="tagtype">Tag type</InputLabel>
-                        <Select label="tagtype" labelId="tagtype">
+                        <Select label="tagtype" labelId="tagtype" onChange={(e) => setTagName(e.target.value)}>
                             {Array.isArray(tagList) && tagList.length
                                 ? tagList.map((tag) => <MenuItem value={tag.value}>{tag.label}</MenuItem>)
                                 : null}
                         </Select>
                     </FormControl>
                 </Grid>
+                <Grid item xs={1} sm={1}>
+                    <FormControl>
+                        <Button variant="contained" type="submit" sx={{ ml: 1, mt: 1 }} onClick={onAddMore}>
+                            Add
+                        </Button>
+                    </FormControl>
+                </Grid>
             </Grid>
             <Grid container sx={{ my: 3, ml: 1 }}>
-                <Grid item xs={12} sm={12}>
-                    {blocks.map((data) => (
-                        <Card variant="outlined" sx={{ px: 1 }}>
-                            <EditableForm data={data} />
-                        </Card>
-                    ))}
-                </Grid>
+                {blocks.map((data, index) => (
+                    <EditableForm data={data} onChange={onChange} index={index} onChangeFile={onChangeFile} addMoreList={addMoreList} />
+                ))}
             </Grid>
             <Grid item xs={12}>
                 <Stack direction="row" justifyContent="space-between">
